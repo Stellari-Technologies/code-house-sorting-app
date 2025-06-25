c7aa3d5fdb7d553c568dd594b9ce0d9df0c37d95
ormle <101660382+ormle@users.noreply.github.com>
2025-06-25 15:24:06 -0600

    Style Quiz
    Complete styling of quiz questions and answering. Quiz functionality next

--- code-house-sorting-quiz/src/components/Quiz.jsx
+++ code-house-sorting-quiz/src/components/Quiz.jsx
@@ -6,10 +6,20 @@ export const Quiz = ({show}) => {
     const [questionIndex, setQuestionIndex] = useState(0)
     const [userName, setUserName] = useState("")
     const [startQuiz, setStartQuiz] = useState(false)
-    //const []
+
+    let userAnswers = {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0,
+    }
 
     const handleStart = () => {
         setStartQuiz(true)
+    }
+
+    const chooseAnswer = ( chosenAnswer ) => {
+        console.log(chosenAnswer)
     }
 
     return (
@@ -26,26 +36,27 @@ export const Quiz = ({show}) => {
             <div className="flex flex-col">
                 {startQuiz === false ?  (
                     <div className="flex flex-col content-center">
-                        <label className="text-6xl mt-8"> Enter your Name: </label>
+                        <label className="text-6xl mt-8"> Enter your name: </label>
                         <input 
                             type="text"
                             id="userName"
+                            placeholder="Name"
                             required
                             maxLength={15}
                             onChange={(e) => setUserName(e.target.value)}
                             className="bg-yellow-100 rounded-2xl p-8 mt-8 text-8xl w-4/5 border-8 border-slate-900 self-center"
                         />
-                        <button onClick={handleStart}className="m-16 text-4xl bg-yellow-500 border-8 border-yellow-300 box-content size-48 self-center" >Next</button>
+                        <button onClick={handleStart}className="m-16 text-4xl bg-yellow-500 border-8 border-yellow-300 min-w-72 min-h-32 self-center hover:border-yellow-500 hover:bg-yellow-300 hover:text-white" >Next</button>
 
                     </div>
                     ):( 
                     <div>
-                        <h1> {data[questionIndex].question}</h1>
-                        <ul className="flex flex-col text-start">
-                            <li >{data[questionIndex].option1}</li>
-                            <li >{data[questionIndex].option2}</li>
-                            <li >{data[questionIndex].option3}</li>
-                            <li >{data[questionIndex].option4}</li>
+                        <h1 className="text-4xl underline decoration-8"> {data[questionIndex].question}</h1>
+                        <ul className="grid grid-cols-2 text-start max-w-1/2 gap-8 mt-16 mx-16">
+                            <button onClick={console.log("hello")} className="text-3xl py-16 px-8 border-8 border-pink-200 rounded-md bg-pink-100 cursor-pointer hover:border-pink-300 active:bg-pink-200">{data[questionIndex].option1}</button>
+                            <button onClick={chooseAnswer("B")} className="text-3xl py-16 px-8 border-8 border-blue-200 rounded-md bg-blue-100 cursor-pointer hover:border-blue-300 active:bg-blue-200">{data[questionIndex].option2}</button>
+                            <button onClick={chooseAnswer("C")} className="text-3xl py-16 px-8 border-8 border-orange-200 rounded-md bg-orange-100 cursor-pointer hover:border-orange-300 active:bg-orange-200">{data[questionIndex].option3}</button>
+                            <button onClick={chooseAnswer("D")} className="text-3xl py-16 px-8 border-8 border-purple-200 rounded-md bg-purple-100 cursor-pointer hover:border-purple-300 active:bg-purple-200">{data[questionIndex].option4}</button>
                         </ul>  
                     </div>
                    
